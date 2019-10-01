6b8dfdf2c1fe7b44d0caf9ab421149e85a2c8651
ariesrooshadie <ariesrooshadie@gmail.com>
2019-09-30 23:11:52 -0700

    pindah semua isi folder phpmvc

---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Mahasiswa_model {
+	private $table = 'mahasiswa';
+	private $db;
+
+	public function __construct()
+	{
+		$this->db = new Database;
+	}
+
+
+	public function getAllMahasiswa()
+	{
+		$this->db->query('SELECT * FROM ' . $this->table);
+		
+		return $this->db->resultSet();
+
+	}
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+	<h1 class ="mt-4">About me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.jpg" alt="Aries Rooshadie" widht = "200" class="shadow">
+		<p>Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya seorang <?= $data['pekerjaan'] ?></p>
+	
+</div>
